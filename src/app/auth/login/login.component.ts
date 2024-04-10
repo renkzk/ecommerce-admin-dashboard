873125828be5a29f8bc67d7b5829ff20302379c8
renhubgit <renik2001@gmail.com>
2024-04-10 21:28:56 +0200

    login form logic & validation

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,21 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
   constructor(private fb: FormBuilder) {}
 
   loginForm = this.fb.group({
-    username: '',
-    password: '',
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
   });
 
+  isSubmitted = false;
+  isLoading = false;
+
+  isInvalid(field: 'username' | 'password'): boolean | undefined {
+    return (
+      this.loginForm.get(field)?.invalid &&
+      (this.loginForm.get(field)?.touched ||
+        this.loginForm.get(field)?.dirty ||
+        this.isSubmitted)
+    );
+  }
+
   onSubmit() {
-    console.log(this.loginForm.value);
+    this.isSubmitted = true;
+    if (this.loginForm.valid) {
+      this.isLoading = true;
+
+      // TODO: implement login
+      setTimeout(() => {
+        this.isLoading = false;
+        console.log(this.loginForm.value);
+      }, 2000);
+    }
   }
 }
