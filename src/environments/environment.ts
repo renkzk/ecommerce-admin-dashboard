7b3cb2e2a20dd890ad1c58bc693db7be672ecb19
renhubgit <renik2001@gmail.com>
2024-04-11 19:59:02 +0200

    added environment variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+};
+
+// When building your Angular application, specify the target environment
+// to do so use the --configuration flag with the Angular CLI commands.
+// Example: ng build --configuration=production
