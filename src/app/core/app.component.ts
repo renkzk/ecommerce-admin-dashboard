bc887fd86ab78b7714c5e4ee62eaaa7ad3233e2c
renhubgit <renik2001@gmail.com>
2024-04-13 23:03:13 +0200

    implemented translation

--- src/app/core/app.component.ts
+++ src/app/core/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from "@angular/router";
 import { LocalStorageKey } from "@app/shared/models/enums/local-storage.enum";
 import { ThemeService } from "./theme/theme.service";
 import { HeaderComponent } from "./header/header.component";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: "app-root",
@@ -16,23 +17,20 @@ import { HeaderComponent } from "./header/header.component";
       </div>
     </div>
   `,
-  styles: [
-    `
-      :host {
-        display: block;
-        height: 100%;
-      }
-    `,
-  ],
 })
 export class AppComponent implements OnInit {
   // Dependencies
   private themeService = inject(ThemeService);
+  private translate = inject(TranslateService);
 
   ngOnInit(): void {
     // set theme (light theme is default if not set in LocalStorage)
     this.themeService.setTheme(
       localStorage.getItem(LocalStorageKey.Theme) === "dark" ? "dark" : "light",
     );
+    // this language will be used as a fallback when a translation isn't found in the current language
+    this.translate.setDefaultLang("en");
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    this.translate.use(navigator.language.split("-")[0]);
   }
 }
--- src/app/core/app.config.ts
+++ src/app/core/app.config.ts
@@ -1,10 +1,12 @@
-import { ApplicationConfig, ErrorHandler } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { CustomErrorHandler } from '../shared/services/custom-error-handler.service';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { ApplicationConfig, ErrorHandler, importProvidersFrom } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app.routes";
+import { HttpClient, provideHttpClient } from "@angular/common/http";
+import { CustomErrorHandler } from "../shared/services/custom-error-handler.service";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +17,20 @@ export const appConfig: ApplicationConfig = {
     // Material Providers
     provideAnimationsAsync(),
     MatSnackBar,
+    // i18n
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      }),
+    ),
   ],
 };
+
+// Factory for creating a TranslateHttpLoader instance.
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+}
