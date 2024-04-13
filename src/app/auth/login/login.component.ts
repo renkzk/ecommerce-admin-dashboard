b49ac447e69e04f8c0cae824855fc225b537f375
renhubgit <renik2001@gmail.com>
2024-04-13 18:03:33 +0200

    login component error handling & other changes

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,43 +1,66 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-
+import { CommonModule } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { AuthService } from "../services/auth.service";
+import { finalize } from "rxjs";
+import { HttpErrorResponse } from "@angular/common/http";
 @Component({
-  selector: 'app-login',
+  selector: "app-login",
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule],
-  templateUrl: './login.component.html',
+  templateUrl: "./login.component.html",
 })
 export class LoginComponent {
-  constructor(private fb: FormBuilder) {}
+  // Injections
+  fb = inject(FormBuilder);
+  authService = inject(AuthService);
 
-  loginForm = this.fb.group({
-    username: ['', [Validators.required]],
-    password: ['', [Validators.required]],
+  // Properties
+  loginForm = this.fb.nonNullable.group({
+    identifier: ["", [Validators.required, Validators.minLength(4)]],
+    password: ["", [Validators.required]],
   });
+
+  get identifier() {
+    return this.loginForm.get("identifier");
+  }
+  get password() {
+    return this.loginForm.get("password");
+  }
 
   isSubmitted = false;
   isLoading = false;
+  error: Error | null = null;
 
-  isInvalid(field: 'username' | 'password'): boolean | undefined {
+  // Methods
+  isInvalid(field: "identifier" | "password"): boolean | undefined {
     return (
       this.loginForm.get(field)?.invalid &&
-      (this.loginForm.get(field)?.touched ||
-        this.loginForm.get(field)?.dirty ||
-        this.isSubmitted)
+      (this.loginForm.get(field)?.touched || this.loginForm.get(field)?.dirty || this.isSubmitted)
     );
   }
 
   onSubmit() {
+    // Reset
     this.isSubmitted = true;
-    if (this.loginForm.valid) {
-      this.isLoading = true;
+    this.error = null;
 
-      // TODO: implement login
-      setTimeout(() => {
-        this.isLoading = false;
-        console.log(this.loginForm.value);
-      }, 2000);
-    }
+    // Return if invalid
+    if (this.loginForm.invalid) return;
+
+    // Else proceed
+    this.isLoading = true;
+
+    this.authService
+      .login(this.loginForm.getRawValue())
+      .pipe(finalize(() => (this.isLoading = false)))
+      .subscribe({
+        next: (res) => {
+          console.log("Login successful");
+        },
+        error: (err: HttpErrorResponse) => {
+          this.error = err.error;
+        },
+      });
   }
 }
